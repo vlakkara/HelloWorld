e3512ba5a743ed9f304ff69bb1806f627cba2622
Visweswararao Lakkaraju <visweswararaolakkaraju@VisweswaraosMBP.lan1>
2018-09-16 11:29:30 -0500

    Hellow World Program

---
+++ HelloWorld.java
@@ -0,0 +1,14 @@
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=0; i<10; i++) {
+			
+			System.out.println("Hello World");
+			
+		}
+		
+	}
+
+}
